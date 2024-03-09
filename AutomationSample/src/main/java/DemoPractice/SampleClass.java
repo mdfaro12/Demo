ad36f04211165d2191ec3016346b788c1756db2f
91812 <91812@MDFARO>
2024-03-09 11:13:15 +0530

    second commit

---
+++ AutomationSample/src/main/java/DemoPractice/SampleClass.java
@@ -0,0 +1,14 @@
+package DemoPractice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SampleClass {
+	
+	@Test
+	public void toSample() {
+		Reporter.log("Good Morning",true);
+		
+	}
+
+}
